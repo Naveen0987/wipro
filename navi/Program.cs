4a44d4c3f740ecc9d77e04dd463d9619712ab9eb
Naveen0987 <119598963+Naveen0987@users.noreply.github.com>
2024-04-16 17:53:22 +0530

    Add files via upload

---
+++ navi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+namespace input{
+    class Samp{
+
+
+
+       static void Main(string[] args){
+     
+
+int [] arr={5,8,6,9,15,10,4};
+int target =10;
+
+for (int i=0;i < arr.Length; i++)
+{
+
+ if(arr[i] == target ) 
+ {
+Console.WriteLine(arr[i]+" element found at index position "+i);
+ }
+
+  else {
+    Console.WriteLine("Not found") ;
+  }
+
+}
+
+
+     /* Console.WriteLine("Hello, World! im navin");
+             Console.WriteLine("enter number");
+             int a=Convert.ToInt16(Console.ReadLine());
+             Console.WriteLine("hello this is the input from user :"+a);
+
+     //method calling
+             this_is_method_1() ;
+       this_is_method_2() ;
+        }
+
+//methods 
+static void this_is_method_1(){
+
+    
+    Console.WriteLine("This is the first method");
+}
+static void this_is_method_2(){
+    Console.WriteLine("this is the second method");
+*/
+
+}
+
+
+       }
+    }   
